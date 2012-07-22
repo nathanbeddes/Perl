57d82532caa8a40f275025265be728a10c81d2c3
Nathan Beddes <beddes@gmail.com>
2012-07-21 23:08:47 -0400

    Finishing dir_walk() and all the functions we can pass in.

--- HD.pl
+++ HD.pl
@@ -9,6 +9,81 @@ sub dir_walk {
 
     if (-d $top) {
 	my $file;
-	
+	unless (opendir $DIR, $top) {
+	    warn "Couldn't open directory $top: $!; skipping.\n";
+	    return;
+	}
+
+	my @results;
+	while ($file = readdir $DIR) {
+	    next if $file eq '.' || $file eq '..';
+	    push @results, dir_walk("$top/$file", $filefunc, $dirfunc);
+	}
+	return $dirfunc ? $dirfunc->($top, @results) : ();
+    } else {
+	return $filefunc ? $filefunc->($top) : ();
     }
 }
+
+
+sub file_size {
+    return (-s $_[0]);
+}
+
+
+sub dir_size {
+    my $dir = shift;
+    my $total = -s $dir;
+
+    my $n;
+    for $n (@_) {
+	$total += $n;
+    }
+    printf "%6d %s\n", $total, $dir;
+
+    return $total;
+}
+
+
+sub file {
+    my $file = shift;
+    [short($file), -s $file];
+}
+
+
+sub short {
+    my $path = shift;
+    $path =~ s{.*/}{};
+    $path;
+}
+
+
+sub dir {
+    my ($dir, @subdirs) = @_;
+    my %new_hash;
+    
+    for (@subdirs) {
+	my ($subdir_name, $subdir_structure) = @$_;
+	$new_hash{$subdir_name} = $subdir_structure;
+    }
+    
+    return [short($dir), \%new_hash];
+}
+
+
+sub print_filename {
+    print "$_[0]\n";
+}
+
+
+sub dangles {
+    my $file = shift;
+    print "$file\n" if -l $file && ! -e $file;
+}
+
+
+my @all_plain_files = dir_walk('.', sub {$_[0]}, sub {shift; return @_;});
+print "@all_plain_files\n";
+
+#my $total_size = dir_walk('.', \&file_size, \&dir_size);
+dir_walk('.', \&dangles);
